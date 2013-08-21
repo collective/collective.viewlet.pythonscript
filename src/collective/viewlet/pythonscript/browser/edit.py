82211e600ad28eb34c7d6387a2eb2ffeaad76180
Radek Jankiewicz <radoslaw.jankiewicz@gmail.com>
2013-08-21 12:03:55 +0200

    development

--- src/collective/viewlet/pythonscript/browser/edit.py
+++ src/collective/viewlet/pythonscript/browser/edit.py
@@ -5,7 +5,10 @@ from Products.CMFPlone import PloneMessageFactory as _
 class Edit(BaseEdit):
 
     def __call__(self, context=None, request=None):
-        inherit = self.fields(['Listing'])[0].get(self.context)
+        try:
+            inherit = self.fields(['Listing'])[0].get(self.context)
+        except KeyError:
+            return self
         if inherit:
             settings_provider = [i for i in self.context.aq_chain \
                 if getattr(i, 'inherit_viewlet_settings', False)][-1].aq_parent
